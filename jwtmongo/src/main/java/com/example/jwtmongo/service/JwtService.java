d23613a884ece3959de5242feea2c936eff384b3
AnanduMahesh <anandumahesh7810@gmail.com>
2025-08-17 12:27:21 +0530

    Modification in dependency and the secret key

--- jwtmongo/src/main/java/com/example/jwtmongo/service/JwtService.java
+++ jwtmongo/src/main/java/com/example/jwtmongo/service/JwtService.java
@@ -2,21 +2,26 @@ package com.example.jwtmongo.service;
 
 import com.example.jwtmongo.model.User;
 import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.SecretKey;
+import java.util.Date;
 @Service
 public class JwtService {
-    public static final String SECRET = "secret-key";
+//    public static final String SECRET = "secret-key";
+    private static final SecretKey SECRET = Keys.hmacShaKeyFor("Super-Secret-key123".getBytes());
     public String generateToken(User user){
         return Jwts.builder()
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis()+86400000))
-                .signWith(SignatureAlgorithm.HS256,SECRET)
+                .signWith(SECRET, SignatureAlgorithm.HS256)
                 .compact();
     }
     public String extractUsername(String token){
